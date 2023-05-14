7a3b62a82f05d9b2d570715e8c852547a8a424ce
malakafaqahmad <124126284+malakafaqahmad@users.noreply.github.com>
2023-05-15 01:24:03 +0500

    first by afaq

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<windows.h>
+#include<string>
+#include <cstdlib>
+#include <conio.h>
+#include <fstream>
+#include <stdlib.h>
+#include <iomanip>
+#include <string.h>
+#include <stdio.h>
+
+using namespace std;
+
+////afaq code from line 8 to line 996.
+//Declare Enums here
+enum Admins{Afaq=1, Rameesa};
+
+//Declare global variables here
+
+bool adminis_Afaq;
+bool adminis_Rameesa;
+
+const int MAX_BREAKFAST = 10;
+const int MAX_LUNCH = 10;
+const int MAX_DINNER = 10;
+const int MAX_BEVERAGE = 10;
+const int MAX_DESSERT = 10;
+
+int numBREAKFAST = 0;
+int numLUNCH = 0;
+int numDINNER = 0;
+int numBEVERAGE = 0;
+int numDESSERT = 0;
+
+int TOTAL_ITEMS_AVAILABLE = 0;
+
